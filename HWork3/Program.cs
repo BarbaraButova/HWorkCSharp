9193f258642c126dca5f6cf6c9c60bcbee4d214d
Barbara_Butova <butova95@ya.ru>
2022-12-07 13:28:30 +0300

    correct Task19

--- HWork3/Program.cs
+++ HWork3/Program.cs
@@ -1,8 +1,8 @@
 ﻿using static System.Console;
 using static HWork3;
 WriteLine("Введите число");
-int number = Convert.ToInt32(ReadLine());
-Task19(number);
-//Задача №19
 
+int number = Convert.ToInt32(ReadLine());
+
+Task19(number);
 
