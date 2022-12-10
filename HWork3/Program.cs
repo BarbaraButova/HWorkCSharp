27e071a181aaa95734e8cc4d91496cfd855970e3
Barbara_Butova <butova95@ya.ru>
2022-12-10 23:16:05 +0300

    исправлен program

--- HWork3/Program.cs
+++ HWork3/Program.cs
@@ -1,11 +1,4 @@
 ﻿using static HWork3;
-//WriteLine("Введите число");
-
-//int number = Convert.ToInt32(ReadLine());
-
-//Task19();
-
-//WriteLine("Введите координаты первой точки");
-//int Xa = Convert.ToInt32(ReadLine());
-//Task21();
+Task19();
+Task21();
 Task29();
