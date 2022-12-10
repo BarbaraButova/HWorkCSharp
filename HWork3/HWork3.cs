dda3cd76e107558db5aa7f274a4de056c5bda7bb
Barbara_Butova <butova95@ya.ru>
2022-12-10 23:08:37 +0300

    задача 29 решена

--- HWork3/HWork3.cs
+++ HWork3/HWork3.cs
@@ -1,3 +1,4 @@
+using static System.Console;
 /// <summary>
 /// Домашняя работа к семинару 3
 /// </summary>
@@ -7,28 +8,59 @@ public static class HWork3
     /// Задача №19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
     /// </summary>
     /// <param name="number">заданное пятизначное число</param>
-    public static void Task19(int number)
+    public static void Task19()
     {
-
-        int n = number;
-        if ((n > 9999) && (n <= 99999))
-        {   int div = Math.DivRem(n, 10, out int digitRight);
+        WriteLine("Введите число");
+        int number = Math.Abs(Convert.ToInt32(ReadLine()));
+        if ((number > 9999) && (number <= 99999))
+        {   int div = Math.DivRem(number, 10, out int digitRight);
             int digitLeft = Math.DivRem(div, 1000, out int remainder);
             if (digitRight == digitLeft)
             {
                div = Math.DivRem(remainder, 10, out digitLeft);
                digitRight = Math.DivRem(div, 10, out remainder);
                if (digitRight == digitLeft)
-               Console.WriteLine("Число является палиндромом");
+               WriteLine("Число является палиндромом");
                else 
-               Console.WriteLine("Число не является палиндромом");
+               WriteLine("Число не является палиндромом");
             }
             else
-            Console.WriteLine("Число не является палиндромом");
+            WriteLine("Число не является палиндромом");
         }     
         else
-        Console.WriteLine("Число не пятизначное");
+        WriteLine("Число не пятизначное");
             
+    }
+    /// <summary>
+    /// Задача №23: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+    /// </summary> 
+   public static void Task21()
+   {
+        WriteLine("Введите координаты первой точки через ';'");
+        string pointA = ReadLine();
+        string[] coordA = pointA.Split(new char[] { ';' });
+        double Xa = Convert.ToDouble(coordA[0]);
+        double Ya = Convert.ToDouble(coordA[1]);
+        WriteLine("Введите координаты второй точки через ;");
+        string pointB = ReadLine();
+        string[] coordB = pointB.Split(new char[] { ';' });
+        double Xb = Convert.ToDouble(coordB[0]);
+        double Yb = Convert.ToDouble(coordB[1]);
+        
+        double distanceAB = Math.Sqrt(Math.Pow((Xb - Xa), 2) + Math.Pow((Yb - Ya), 2));
+        Console.WriteLine($"Расстояние между точками = {distanceAB}");
+    }
+    /// <summary>
+    /// Задача №29: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+    /// </summary>
+    public static void Task29()
+    {
+        WriteLine("Введите число");
+        int N = Math.Abs(Convert.ToInt32(ReadLine()));
+        for (int num = 1; num <= N; num++)
+        {
+            WriteLine($"{num}^3 = {Math.Pow(num, 3)}");
+        }
     }
 }
     
--- HWork3/Program.cs
+++ HWork3/Program.cs
@@ -1,8 +1,11 @@
-﻿using static System.Console;
-using static HWork3;
-WriteLine("Введите число");
+﻿using static HWork3;
+//WriteLine("Введите число");
 
-int number = Convert.ToInt32(ReadLine());
+//int number = Convert.ToInt32(ReadLine());
 
-Task19(number);
+//Task19();
 
+//WriteLine("Введите координаты первой точки");
+//int Xa = Convert.ToInt32(ReadLine());
+//Task21();
+Task29();
