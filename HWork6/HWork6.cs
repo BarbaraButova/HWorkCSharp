be5226595a31d67b4eca205cebf356caf0f86be4
Barbara_Butova <butova95@ya.ru>
2022-12-24 13:24:04 +0300

    номер задачи 41, а не 34

--- HWork6/HWork6.cs
+++ HWork6/HWork6.cs
@@ -1,10 +1,13 @@
-
+/// <summary>
+/// Домашняя работа 6
+/// <para> Задача 41 - Task41 </para>
+/// </summary>
 public static class HWork6
 {
 /// <summary>
 /// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 /// </summary>
-    public static void Task34()
+    public static void Task41()
     {
         Console.WriteLine("Введите количество чисел");
         int[] numbers = LibraryForArray.CreateArray(Convert.ToInt32(Console.ReadLine()));
